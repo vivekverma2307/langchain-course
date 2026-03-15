71bfbb047b0cf5ffaa86cd8cd1c4c4128e55ad69
Vivek Verma <vivek.vrm1@gmail.com>
2026-03-15 00:04:36 -0400

    Langrapah agent

--- main.py
+++ main.py
@@ -5,23 +5,17 @@ from langchain.agents import create_agent
 from langchain.tools import tool
 from langchain_core.messages import HumanMessage
 from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_tavily import TavilySearch, tavily_search
 
-@tool
-def search(query:str) ->str:
-    """
-    "Returns the current weather for a given location.
-    """
-    print(f"Searching  for {query}")
-    return "Tokyo weather is sunny"
 
 llm=ChatGoogleGenerativeAI(model="gemini-2.5-flash")
-tools=[search]
+tools=[TavilySearch()]
 agent=create_agent(model=llm,tools=tools)
 
 
 def main():
     print("Hello from langchain-course!")
-    result=agent.invoke({"messages":[HumanMessage(content="what is the weather in Tokyo")]})
+    result=agent.invoke({"messages":[HumanMessage(content="Search for top 10 MES job opening in canada which are in S&P 500 or similer comapny. search only active jobs that are accepting application")]})
     print(result)
 
 
